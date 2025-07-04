e79dffcfb4167bf3d1c8a5e7be8739842d867ea1
Mateo Llerena <mateollerena40@gmail.com>
2025-07-04 11:31:29 -0500

    Integracion basica webview

---
+++ WoWonder/Activities/Tab/Fragment/HomeFragment.cs
@@ -0,0 +1,68 @@
+﻿using Android.OS;
+using Android.Views;
+using Android.Webkit;
+using System.Reflection;
+using WoWonder.Helpers.Model;
+
+namespace WoWonder.Activities.Tab.Fragment
+{
+    public class HomeFragment : AndroidX.Fragment.App.Fragment
+    {
+        private const string LogTag = "ZUITCH_APP";
+        private const string BaseUrl = "https://zuitch.com/";
+        private WebView _web;
+
+        public override View OnCreateView(LayoutInflater inflater,
+                                          ViewGroup container,
+                                          Bundle savedInstanceState)
+        {
+            var root = inflater.Inflate(Resource.Layout.fragment_home, container, false);
+            _web = root.FindViewById<WebView>(Resource.Id.web_home);
+
+            DumpUserDetails();   // imprime todas las variables
+            InitWebView();       // configura y carga la página
+
+            return root;
+        }
+
+        /* ---------- helpers ---------- */
+
+        private void DumpUserDetails()
+        {
+            foreach (var f in typeof(UserDetails)
+                     .GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var v = f.GetValue(null) ?? "null";
+                Android.Util.Log.Info(LogTag, $"{f.Name} = {v}");
+                System.Diagnostics.Debug.WriteLine($"{LogTag}: {f.Name} = {v}");
+            }
+        }
+
+        private void InitWebView()
+        {
+            var st = _web.Settings;
+            st.JavaScriptEnabled = true;
+            st.DomStorageEnabled = true;
+
+            // Mantener toda la navegación dentro del WebView
+            _web.SetWebViewClient(new InsideClient());
+
+            _web.LoadUrl(BaseUrl);
+        }
+
+        /* ---------- WebViewClient mínimo ---------- */
+
+        private class InsideClient : WebViewClient
+        {
+            // Para Android ≥ API-24
+            public override bool ShouldOverrideUrlLoading(WebView view,
+                                                          IWebResourceRequest request)
+                => false;    // siempre cargar en el WebView
+
+            // Para Android < API-24
+            public override bool ShouldOverrideUrlLoading(WebView view,
+                                                          string url)
+                => false;
+        }
+    }
+}
--- WoWonder/Activities/Tab/Fragment/TabChatFragment.cs
+++ WoWonder/Activities/Tab/Fragment/TabChatFragment.cs
@@ -16,12 +16,16 @@ namespace WoWonder.Activities.Tab.Fragment
         private ViewPager2 ViewPager;
         private MainTabAdapter TabAdapter;
         private LinearLayout Tab;
-        private TextView TxtChats, TxtGroups, TxtArchives;
-        private LinearLayout ButtonChats, ButtonGroups, ButtonArchives;
 
-        public LastChatFragment LastChatTab;
-        public LastGroupChatsFragment LastGroupChatsTab;
-        public ArchivedChatsFragment ArchivedChatsTab;
+        // Botones y textos de las pestañas
+        private LinearLayout ButtonHome, ButtonChats, ButtonGroups, ButtonArchives;
+        private TextView    TxtHome,    TxtChats,  TxtGroups,  TxtArchives;
+
+        // Fragmentos
+        public HomeFragment            HomeTab;
+        public LastChatFragment        LastChatTab;
+        public LastGroupChatsFragment  LastGroupChatsTab;
+        public ArchivedChatsFragment   ArchivedChatsTab;
 
         #endregion
 
@@ -31,8 +35,7 @@ namespace WoWonder.Activities.Tab.Fragment
         {
             try
             {
-                View view = inflater.Inflate(Resource.Layout.TabChatLayout, container, false);
-                return view;
+                return inflater.Inflate(Resource.Layout.TabChatLayout, container, false);
             }
             catch (Exception e)
             {
@@ -43,54 +46,10 @@ namespace WoWonder.Activities.Tab.Fragment
 
         public override void OnViewCreated(View view, Bundle savedInstanceState)
         {
-            try
-            {
-                base.OnViewCreated(view, savedInstanceState);
+            base.OnViewCreated(view, savedInstanceState);
 
-                InitComponent(view);
-                AddFragmentsTabs();
-            }
-            catch (Exception e)
-            {
-                Methods.DisplayReportResultTrack(e);
-            }
-        }
-
-        public override void OnResume()
-        {
-            try
-            {
-                base.OnResume();
-            }
-            catch (Exception e)
-            {
-                Methods.DisplayReportResultTrack(e);
-            }
-        }
-
-        public override void OnLowMemory()
-        {
-            try
-            {
-                GC.Collect(GC.MaxGeneration);
-                base.OnLowMemory();
-            }
-            catch (Exception e)
-            {
-                Methods.DisplayReportResultTrack(e);
-            }
-        }
-
-        public override void OnDestroy()
-        {
-            try
-            {
-                base.OnDestroy();
-            }
-            catch (Exception e)
-            {
-                Methods.DisplayReportResultTrack(e);
-            }
+            InitComponent(view);
+            AddFragmentsTabs();
         }
 
         #endregion
@@ -102,24 +61,29 @@ namespace WoWonder.Activities.Tab.Fragment
             try
             {
                 TabAdapter = new MainTabAdapter(this);
-                ViewPager = view.FindViewById<ViewPager2>(Resource.Id.viewPagerChat);
-                Tab = view.FindViewById<LinearLayout>(Resource.Id.tabsChat);
 
-                ButtonChats = view.FindViewById<LinearLayout>(Resource.Id.llChats);
-                ButtonGroups = view.FindViewById<LinearLayout>(Resource.Id.llGroups);
+                ViewPager = view.FindViewById<ViewPager2>(Resource.Id.viewPagerChat);
+                Tab       = view.FindViewById<LinearLayout>(Resource.Id.tabsChat);
+
+                // Asignar controles
+                ButtonHome     = view.FindViewById<LinearLayout>(Resource.Id.llHome);
+                ButtonChats    = view.FindViewById<LinearLayout>(Resource.Id.llChats);
+                ButtonGroups   = view.FindViewById<LinearLayout>(Resource.Id.llGroups);
                 ButtonArchives = view.FindViewById<LinearLayout>(Resource.Id.llArchive);
 
-                TxtChats = view.FindViewById<TextView>(Resource.Id.ivChat);
-                TxtGroups = view.FindViewById<TextView>(Resource.Id.ivGroups);
+                TxtHome     = view.FindViewById<TextView>(Resource.Id.ivHome);
+                TxtChats    = view.FindViewById<TextView>(Resource.Id.ivChat);
+                TxtGroups   = view.FindViewById<TextView>(Resource.Id.ivGroups);
                 TxtArchives = view.FindViewById<TextView>(Resource.Id.ivArchive);
 
-                if (!ButtonChats.HasOnClickListeners)
+                // Eventos
+                if (!ButtonHome.HasOnClickListeners)
                 {
-                    ButtonChats.Click += ButtonChats_Click;
-                    ButtonGroups.Click += ButtonGroups_Click;
+                    ButtonHome.Click     += ButtonHome_Click;
+                    ButtonChats.Click    += ButtonChats_Click;
+                    ButtonGroups.Click   += ButtonGroups_Click;
                     ButtonArchives.Click += ButtonArchives_Click;
                 }
-
             }
             catch (Exception e)
             {
@@ -127,16 +91,13 @@ namespace WoWonder.Activities.Tab.Fragment
             }
         }
 
-        #endregion
-
-        #region Set Tab
-
         private void AddFragmentsTabs()
         {
             try
             {
                 TabAdapter.ClaerFragment();
 
+                HomeTab     = new HomeFragment();
                 LastChatTab = new LastChatFragment();
 
                 if (AppSettings.EnableChatGroup)
@@ -145,96 +106,89 @@ namespace WoWonder.Activities.Tab.Fragment
                 if (AppSettings.EnableChatArchive)
                     ArchivedChatsTab = new ArchivedChatsFragment();
 
-                if (TabAdapter is { ItemCount: <= 0 })
-                {
-                    TabAdapter.AddFragment(LastChatTab, GetText(Resource.String.Lbl_User));
+                // Añadir fragmentos al adaptador
+                TabAdapter.AddFragment(HomeTab,     GetText(Resource.String.Lbl_Home));   // 0
+                TabAdapter.AddFragment(LastChatTab, GetText(Resource.String.Lbl_User));   // 1
 
-                    if (AppSettings.EnableChatGroup)
-                        TabAdapter.AddFragment(LastGroupChatsTab, GetText(Resource.String.Lbl_Group));
+                if (AppSettings.EnableChatGroup)
+                    TabAdapter.AddFragment(LastGroupChatsTab, GetText(Resource.String.Lbl_Group)); // 2
 
-                    if (AppSettings.EnableChatArchive)
-                        TabAdapter.AddFragment(ArchivedChatsTab, GetText(Resource.String.Lbl_Archive));
+                if (AppSettings.EnableChatArchive)
+                    TabAdapter.AddFragment(ArchivedChatsTab,  GetText(Resource.String.Lbl_Archive)); // 3
 
-                    ViewPager.UserInputEnabled = false;
-                    ViewPager.CurrentItem = TabAdapter.ItemCount;
-                    ViewPager.OffscreenPageLimit = TabAdapter.ItemCount;
+                // Configurar ViewPager
+                ViewPager.UserInputEnabled   = false;
+                ViewPager.OffscreenPageLimit = TabAdapter.ItemCount;
+                ViewPager.Orientation        = ViewPager2.OrientationHorizontal;
+                ViewPager.Adapter            = TabAdapter;
+                ViewPager.Adapter.NotifyDataSetChanged();
 
-                    ViewPager.Orientation = ViewPager2.OrientationHorizontal;
-                    ViewPager.Adapter = TabAdapter;
-                    ViewPager.Adapter.NotifyDataSetChanged();
-                }
+                // Abrir por defecto en «Chats» (índice 1)
+                ViewPager.CurrentItem = 1;
+                ResaltarPestaña(ButtonChats, TxtChats);
             }
-            catch (Exception exception)
+            catch (Exception e)
             {
-                Methods.DisplayReportResultTrack(exception);
+                Methods.DisplayReportResultTrack(e);
             }
+        }
+
+        /// <summary>
+        /// Limpia estilos de todas las pestañas.
+        /// </summary>
+        private void ResetTabs()
+        {
+            ButtonHome.SetBackgroundResource(0);
+            ButtonChats.SetBackgroundResource(0);
+            ButtonGroups.SetBackgroundResource(0);
+            ButtonArchives.SetBackgroundResource(0);
+
+            var normal = Resources.GetColor(Resource.Color.text_color_in_between, null);
+            TxtHome.SetTextColor(normal);
+            TxtChats.SetTextColor(normal);
+            TxtGroups.SetTextColor(normal);
+            TxtArchives.SetTextColor(normal);
+        }
+
+        /// <summary>
+        /// Aplica estilo seleccionado a la pestaña activa.
+        /// </summary>
+        private void ResaltarPestaña(LinearLayout btn, TextView txt)
+        {
+            btn.SetBackgroundResource(Resource.Drawable.bg_tab_button);
+            txt.SetTextColor(Color.White);
         }
 
         #endregion
 
         #region Event
 
+        private void ButtonHome_Click(object sender, EventArgs e)
+        {
+            ResetTabs();
+            ResaltarPestaña(ButtonHome, TxtHome);
+            ViewPager.SetCurrentItem(0, false);
+        }
+
         private void ButtonChats_Click(object sender, EventArgs e)
         {
-            try
-            {
-                ButtonGroups.SetBackgroundResource(0);
-                ButtonArchives.SetBackgroundResource(0);
-                TxtGroups.SetTextColor(Resources.GetColor(Resource.Color.text_color_in_between, null));
-                TxtArchives.SetTextColor(Resources.GetColor(Resource.Color.text_color_in_between, null));
-
-                ButtonChats.SetBackgroundResource(Resource.Drawable.bg_tab_button);
-                TxtChats.SetTextColor(Color.White);
-
-                //Show UsersList 
-                ViewPager.SetCurrentItem(0, false);
-            }
-            catch (Exception exception)
-            {
-                Methods.DisplayReportResultTrack(exception);
-            }
+            ResetTabs();
+            ResaltarPestaña(ButtonChats, TxtChats);
+            ViewPager.SetCurrentItem(1, false);
         }
 
         private void ButtonGroups_Click(object sender, EventArgs e)
         {
-            try
-            {
-                ButtonChats.SetBackgroundResource(0);
-                ButtonArchives.SetBackgroundResource(0);
-                TxtChats.SetTextColor(Resources.GetColor(Resource.Color.text_color_in_between, null));
-                TxtArchives.SetTextColor(Resources.GetColor(Resource.Color.text_color_in_between, null));
-
-                ButtonGroups.SetBackgroundResource(Resource.Drawable.bg_tab_button);
-                TxtGroups.SetTextColor(Color.White);
-
-                //Show GroupsList 
-                ViewPager.SetCurrentItem(1, false);
-            }
-            catch (Exception exception)
-            {
-                Methods.DisplayReportResultTrack(exception);
-            }
+            ResetTabs();
+            ResaltarPestaña(ButtonGroups, TxtGroups);
+            ViewPager.SetCurrentItem(2, false);
         }
 
         private void ButtonArchives_Click(object sender, EventArgs e)
         {
-            try
-            {
-                ButtonChats.SetBackgroundResource(0);
-                ButtonGroups.SetBackgroundResource(0);
-                TxtChats.SetTextColor(Resources.GetColor(Resource.Color.text_color_in_between, null));
-                TxtGroups.SetTextColor(Resources.GetColor(Resource.Color.text_color_in_between, null));
-
-                ButtonArchives.SetBackgroundResource(Resource.Drawable.bg_tab_button);
-                TxtArchives.SetTextColor(Color.White);
-
-                //Show Archive page 
-                ViewPager.SetCurrentItem(2, false);
-            }
-            catch (Exception exception)
-            {
-                Methods.DisplayReportResultTrack(exception);
-            }
+            ResetTabs();
+            ResaltarPestaña(ButtonArchives, TxtArchives);
+            ViewPager.SetCurrentItem(3, false);
         }
 
         #endregion
